cd6c49d43578c8462e5012b4e41410766bade72b
Huskies Prog <huskies5439@polych.ca>
2023-01-19 13:20:17 -0500

    REsolve push pull conflict

--- src/main/java/frc/robot/subsystems/BasePilotable.java
+++ src/main/java/frc/robot/subsystems/BasePilotable.java
@@ -8,7 +8,9 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -26,9 +28,27 @@ public class BasePilotable extends SubsystemBase {
 
   private Encoder encodeurG = new Encoder(0, 1, true);
   private Encoder encodeurD = new Encoder(2, 3, false); 
-  private double conversionEncodeur;
-
+  private double conversionEncodeur;                                                           //trouver le bon port
+  private DoubleSolenoid pistonTransmission = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 0); 
+  private boolean isHighGear = false;
   private PigeonIMU gyro = new PigeonIMU(5);
+  private double[] ypr = new double[3];
+  public void conduire(double vx, double vz){
+    drive.arcadeDrive(-0.85*vx, 0.7*vz);
+  }
+
+
+  public void autoConduire(double voltGauche, double voltDroit){
+    moteursG.setVoltage(voltGauche);
+    moteursD.setVoltage(voltDroit);
+    drive.feed();
+  }
+
+public void stop(){
+  drive.arcadeDrive(0, 0);
+}
+
+  
 
   /** Creates a new BasePilotable. */
   public BasePilotable() {
@@ -36,6 +56,62 @@ public class BasePilotable extends SubsystemBase {
     encodeurG.setDistancePerPulse(conversionEncodeur);
     encodeurD.setDistancePerPulse(conversionEncodeur);
   }
+<<<<<<< Updated upstream
+=======
+
+  public double getPositionG() {
+    return encodeurG.getDistance();
+  }
+
+  public double getPositionD(){
+    return encodeurD.getDistance();
+  } 
+
+  public double getPosition(){
+    return (getPositionG()+getPositionD())/2.0;
+  }
+
+  public double getVitesseD() {
+    return encodeurD.getRate();
+  }
+
+  public double getVitesseG() {
+    return encodeurG.getRate();
+  }
+
+  public double getVitesse() {
+    return (getVitesseD() + getVitesseG()) / 2.0;
+  }
+
+  public void resetEncodeur() {
+   encodeurD.reset();
+   encodeurG.reset();
+  }
+ public void resetGyro() {
+   gyro.setYaw(0);
+ }
+
+ public boolean getIsHighGear(){
+  return isHighGear;
+ }
+
+ public void highGear(){
+  pistonTransmission.set(DoubleSolenoid.Value.kForward); 
+
+  isHighGear = true;
+ }
+
+ public void lowGear(){
+pistonTransmission.set(DoubleSolenoid.Value.kReverse);
+
+isHighGear = false;
+ }
+
+ public double getAngle() {
+gyro.getYawPitchRoll(ypr);
+return ypr[0];
+ }
+>>>>>>> Stashed changes
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
